e0939a3cc15ed8abdca1d37816696c9cb916af99
Adhemar <adhemar@xagax.com>
2012-04-27 17:00:19 -0300

    last review of Adhemar

--- js/campaign.js
+++ js/campaign.js
@@ -1,52 +1,56 @@
-var exist_form_attempt = false;
-var user_id = jQuery.cookie("user_id"); 
-var url = window.location.href;
-
-function form_attempt(field){
-  if(!exist_form_attempt){
-    var event_info = '{"field":"' + field +'"}';
-         
-    var text = 'event_type=form_attepmt'
-    + '&user_id=' + user_id
-    + '&url=' + url
-    + '&event_info=' + event_info;
-            
-    jQuery.ajax({ 
-      data: text, 
-      type: "GET", 
-      url: "campaign-capture", 
-      success: function(data){}
-    });
-    exist_form_attempt = true;
-  }
-}
-function form_submit(form){
-
-    var event_info = jQuery.toJSON(form.serializeObject());
-    
-    var text = 'event_type=form_submit'
-    + '&user_id=' + user_id
-    + '&url=' + url
-    + '&event_info=' + event_info;
-            
-    jQuery.ajax({ 
-      data: text, 
-      type: "GET", 
-      url: "campaign-capture", 
-      success: function(data){}
-    });
-}
-
-
 jQuery(document).ready(function() {
-  
+
+  var exist_form_attempt = false;
+  var user_id = jQuery.cookie("user_id"); 
+  var url = window.location.href;
+
+  function form_attempt(field){
+    if(!exist_form_attempt){
+      var event_info = '{"field":"' + field +'"}';
+
+      var text = 'event_type=form_attepmt'
+      + '&user_id=' + user_id
+      + '&url=' + url
+      + '&event_info=' + event_info;
+
+      jQuery.ajax({ 
+        data: text, 
+        type: "GET", 
+        url: "/campaign-capture", 
+        success: function(data){}
+      });
+      exist_form_attempt = true;
+    }
+  }
+  function form_submit(form,button){
+
+      var event_info = jQuery.toJSON(form.serializeObject());
+
+      var text = 'event_type=form_submit'
+      + '&user_id=' + user_id
+      + '&url=' + url
+      + '&event_info=' + event_info;
+
+      jQuery.ajax({ 
+        data: text, 
+        type: "GET", 
+        url: "/campaign-capture", 
+        success: function(data){
+          form.unbind('submit');
+          button.unbind('click');
+          button.click();
+        }
+      });
+      
+  }
+
+
   var user_agent = '';
   jQuery.each(jQuery.browser, function(i, val) {
     user_agent = user_agent + ' ' + i + ':' + val;
   });
   
-  
-  jQuery.getJSON("http://jsonip.appspot.com?callback=?",function(data){
+  jQuery.getJSON('/campaign-getip', function(data){
     var ip = data.ip;
     
     if (user_id == null){
@@ -62,11 +66,12 @@ jQuery(document).ready(function() {
     + '&user_id=' + user_id
     + '&referrer=' + referrer;
     
+    
     //Capturing user    
     jQuery.ajax({ 
       data: text, 
       type: "GET", 
-      url: "campaign-capture", 
+      url: "/campaign-capture", 
       success: function(data){}
     });
     
@@ -74,15 +79,17 @@ jQuery(document).ready(function() {
     + '&user_id=' + user_id
     + '&url=' + url;
     
+    
     //Capturing impresion event 
     jQuery.ajax({ 
       data: text, 
       type: "GET", 
-      url: "campaign-capture", 
+      url: "/campaign-capture", 
       success: function(data){}
     });
    
   });
+  
   
   //Form attempt
   jQuery(':input[type=text]').keyup(function() {
@@ -94,10 +101,21 @@ jQuery(document).ready(function() {
   
   //Form Submit
   jQuery('form').submit(function() {
-    var form = jQuery(this);
-    form_submit(form);
+    //I am blocking the submit due to firefox interrupt the ajax call when the form is submit.
+    return false;
   });
-  
-  
+
+  jQuery(':input[type=submit]').click(function() {
+    //The submit is called after the ajax call when the submit button is clicked
+    var button = jQuery(this);
+    var form = button.closest('form');
+    form_submit(form,button);
+  });
+  jQuery(':input[type=image]').click(function() {
+    //The submit is called after the ajax call when the submit button is clicked
+    var button = jQuery(this);
+    var form = button.closest('form');
+    form_submit(form,button);
+  });
   
 });  
